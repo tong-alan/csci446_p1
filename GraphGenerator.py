642070e3d1e32140a4e8a67882965e4ea0aceb39
lsgilder <saltywalrus@gmail.com>
2020-01-31 18:22:29 -0700

    Works now, I guess ¯\_(ツ)_/¯

--- GraphGenerator.py
+++ GraphGenerator.py
@@ -45,58 +45,58 @@ class GraphGenerator(object):
         print(" ")
 
 
-        while more == True:
-            start = secrets.randbelow(self.size)
-            self.addEdge(start, secrets.randbelow(self.size))
-            more = self.checkPlanar(start)
+   #     while more == True:
+   #         start = secrets.randbelow(self.size)
+   #         self.addEdge(start, secrets.randbelow(self.size))
+   #         more = self.checkPlanar(start)
         return
 
 
     #check planarity w/ Euler's Theorem
-    def checkPlanar(self, start):
-        if self.countEdges == 0:
-            return True
-        marked = [0] * self.size
-        parent = [[]] * self.size
-        self.countFaces(-1, start, marked, parent)
-        edges = self.countEdges()
-        print(self.countFaces, self.countEdges, self.size)
+    #def checkPlanar(self, start):
+    #    if self.countEdges == 0:
+    #        return True
+    #    marked = [0] * self.size
+    #    parent = [[]] * self.size
+    #    self.countFaces(-1, start, marked, parent)
+    #    edges = self.countEdges()
+    #    print(self.countFaces, self.countEdges, self.size)
 
-        #Euler's
-        if ((self.faces)-(edges)+(self.size)) == 2:
-            return True
-        else:
-            return False
+    #    #Euler's
+    #    if ((self.faces)-(edges)+(self.size)) == 2:
+    #        return True
+    #    else:
+    #        return False
 
-    #count cycles w/ dfs
-    def countFaces(self,prev, start, marked, parent):  
-        print(start)
-        print(marked[start])
-        print(self.faces)
-        print(parent[start])
-        if marked[start]==2:
-            return self.faces
+    ##count cycles w/ dfs
+    #def countFaces(self,prev, start, marked, parent):  
+    #    print(start)
+    #    print(marked[start])
+    #    print(self.faces)
+    #    print(parent[start])
+    #    if marked[start]==2:
+    #        return self.faces
 
-        #vertex has been visited, so there must be a cycle
-        if marked == 1:
-            print("working!")
-            self.faces += 1
-            return self.faces
-        parent[start].append(prev)
-        print(prev)
+    #    #vertex has been visited, so there must be a cycle
+    #    if marked == 1:
+    #        print("working!")
+    #        self.faces += 1
+    #        return self.faces
+    #    parent[start].append(prev)
+    #    print(prev)
 
-        marked[start] = 1
+    #    marked[start] = 1
 
-        #add adjacent edges to stack
-        for i in range(0,self.size):
-            if self.adjMatrix[start][i] == 1 and prev != i and marked[i] != 2:
-                if parent[start].count(i) < 2:
-                    self.faces = self.countFaces(start, i, marked, parent)
-            else:
-                continue
+    #    #add adjacent edges to stack
+    #    for i in range(0,self.size):
+    #        if self.adjMatrix[start][i] == 1 and prev != i and marked[i] != 2:
+    #            if parent[start].count(i) < 2:
+    #                self.faces = self.countFaces(start, i, marked, parent)
+    #        else:
+    #            continue
 
-        marked[start] = 2
-        return self.faces
+    #    marked[start] = 2
+    #    return self.faces
     
     #returns number of edges
     def countEdges(self):
