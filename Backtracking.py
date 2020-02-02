a0f4759b22d0ac946edef2c03cf3d18126ad24eb
Alan Tong <alantong1068@gmail.com>
2020-02-01 19:18:40 -0700

    added to backtracking

--- Backtracking.py
+++ Backtracking.py
@@ -2,23 +2,27 @@
 
 class Backtracking(object):
     def __init__(self, graph, n_coloring):
+        self.domain = [i for i in range(n_coloring)]
         self.graph = graph
-        self.map = self.color()
         self.n = n_coloring
 
     def print(self):
         for node in self.graph.nodeMatrix:
             print(node)
 
+    def isSafe(self, node, color):
+        for connected_nodes in self.graph.nodeMatrix:
+            if connected_nodes.color is color:
+                return False
+        return True
+
+
     def color(self):
-        for node in self.graph.nodeMatrix:
-            pass
-        map = []
-        for row in range(len(self.graph.adjMatrix)):
-            map.append([self.graph.adjMatrix[row], row])
+        colors = [None for i in range(len(self.graph.nodeMatrix))]
+
 
     def backtracking(self):
-        pass
+
 
 
 
--- Driver.py
+++ Driver.py
@@ -3,11 +3,12 @@ from Backtracking import Backtracking
 
 
 def main():
-    graph10 = GraphGenerator(10)
+    graph10 = GraphGenerator(5)
     graph10.printMatrix()
     print(" -------------------------------- ")
     bt = Backtracking(graph10, 3)
     bt.print()
+    bt.color()
 
 
 
