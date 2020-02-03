a1d4aa15ba373ded7fc6cabe5466f2c97a48bfc6
Alan Tong <alantong1068@gmail.com>
2020-02-03 13:45:22 -0700

    started simmulated annealing

--- Driver.py
+++ Driver.py
@@ -2,24 +2,27 @@ from GraphGenerator import GraphGenerator
 from Backtracking import Backtracking
 from GeneticAlgorithm import GA
 from search import ArcConsistency
+from SimulatedAnnealing import SimulatedAnnealing
 
 
 def main():
     k_coloring = 4
-    nodes = 20
-
+    nodes = 5
+    #
     graph = GraphGenerator(nodes)
-
-    bt = Backtracking(graph, k_coloring)
-    bt.backtracking()
-    bt.print()
-
-    ac = ArcConsistency(graph, k_coloring)
-    ac.backtracking()
-    ac.print()
-
-    ga = GA(graph, k_coloring)
-    ga.train()
+    #
+    # bt = Backtracking(graph, k_coloring)
+    # bt.backtracking()
+    # bt.print()
+    #
+    # ac = ArcConsistency(graph, k_coloring)
+    # ac.backtracking()
+    # ac.print()
+    #
+    # ga = GA(graph, k_coloring)
+    # ga.train()
+    sa = SimulatedAnnealing(graph, k_coloring)
+    sa.simulate()
 
 
 main()
--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -1,2 +1,60 @@
+import random
+from copy import copy
+
 class SimulatedAnnealing(object):
-    pass
+    def __init__(self, graph, k_coloring):
+        self.graph = graph
+        self.k = k_coloring
+        self.temperature = None
+        self.current = None
+
+    def random_start(self):
+        coloring = [random.randint(1, self.k) for i in range(len(self.graph.nodeMatrix))]
+        for i, node in enumerate(self.graph.nodeMatrix):
+            self.graph.nodeMatrix[i].color = coloring[i]
+        self.current = coloring
+        return coloring
+
+    def find_neighbor(self):
+        coloring = copy(self.current)
+        index = random.randint(0, len(self.graph.nodeMatrix) - 1)
+        color = random.randint(1, self.k)
+        coloring[index] = color
+        return coloring
+
+    def calc_fitness(self):
+        num_conflicts = self.k
+        for node in self.graph.nodeMatrix:
+            for edge in node.edges:
+                if node.color == edge.color:
+                    num_conflicts += 1
+        return num_conflicts
+
+    def calc_single_fitness(self, vector):
+        for i, node in enumerate(self.graph.nodeMatrix):
+            self.graph.nodeMatrix[i].color = vector[i]
+        return self.calc_fitness()
+
+    def get_current_coloring(self):
+        return self.current
+
+
+    def simulate(self):
+        original_coloring = self.random_start()
+        original_fitness = self.calc_single_fitness(original_coloring)
+
+        neighbor_coloring = self.find_neighbor()
+        neighbor_fitness = self.calc_single_fitness(neighbor_coloring)
+        if original_fitness >= neighbor_fitness:
+            self.current = original_coloring
+
+
+
+
+
+
+
+
+
+
+
